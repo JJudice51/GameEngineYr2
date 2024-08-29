4ee24eeedc14c52bce21d6d79777373aa36886fa
Justin Judice <142815014+JJudice51@users.noreply.github.com>
2024-08-29 11:02:39 -0500

    Added in C++ Math Library Files

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include "raylib.h"
 
 
 
+//argv 0 will always be the name of the program.
+//argument count
+//argument value
 
-
-int main()
+int main(int argc, char* argv[])
 {
-   
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
